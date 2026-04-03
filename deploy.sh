73927b7abe327c022abc7ea138ac68705045ba7c
PedroHenriquedp <pereira.henrique@aluno.ufabc.edu.br>
2026-04-03 18:40:18 -0300

    ci: adiciona script de automacao de deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "🚀 Iniciando atualização do servidor HTTP..."
+
+cd ~/source-http || exit
+
+echo "⬇️  Puxando as alterações do repositório..."
+git pull
+
+echo "🏗️  Reconstruindo e reiniciando o container..."
+docker compose up -d --build
+
+echo "🧹 Limpando lixo e imagens órfãs do Docker..."
+docker image prune -f
+
+echo "✅ Deploy finalizado com sucesso! Seu servidor está atualizado."
